c94b208d0bed9519720988e0e6b4404553959e9b
Simple Chance <cwjewell19@gmail.com>
2025-02-18 14:27:50 -0500

    Added initial login route

--- client/src/App.js
+++ client/src/App.js
@@ -11,41 +11,23 @@ import React, { useEffect, useState } from 'react';
 export default function App() {
   // Component states for backend connection status, name input, and server response
   const [status, setStatus] = useState('Connecting...');
-  const [name, setName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
   const [response, setResponse] = useState('');
 
   // Functions to send name to the server and update response state with altered name
   // post() routes use async/await and are invoked when the user interacts with the UI
-  const convertToUpper = async () => {
+  const sendLoginInfo = async () => {
     try {
-      const res = await fetch('/api/uppercase', {
+      const res = await fetch('/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name })
+        body: JSON.stringify({ userName, password })
       });
       
       const data = await res.json();
       if (res.ok) {
-        setResponse(data.modifiedName);
-      } else {
-        setResponse(data.error);
-      }
-    } catch (error) {
-      setResponse('Error sending data to server');
-    }
-  };
-
-  const convertToLower = async () => {
-    try {
-      const res = await fetch('/api/lowercase', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name })
-      });
-      
-      const data = await res.json();
-      if (res.ok) {
-        setResponse(data.modifiedName);
+        setResponse(data.body);
       } else {
         setResponse(data.error);
       }
@@ -69,16 +51,21 @@ export default function App() {
     <div className="container">
       <h1>CS310 Patient Portal System</h1>
       <p>Backend Status: {status}</p>
-        <h2>Enter your name:</h2>
+        <h2>Enter your login info:</h2>
         <input 
           type="text" 
-          value={name} 
-          onChange={(e) => setName(e.target.value)} 
-          placeholder="Type your name"
+          value={userName} 
+          onChange={(e) => setUserName(e.target.value)} 
+          placeholder="Type your user name"
         />
-        <button onClick={convertToUpper}>Convert to upper</button>
-        <button onClick={convertToLower}>Convert to lower</button>
-          <h3>Modified Name:</h3>
+        <input 
+          type="text" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          placeholder="Type your password"
+        />
+        <button onClick={sendLoginInfo}>Submit</button>
+          <h3>Login Info:</h3>
           <p>{response || "Waiting for input..."}</p>
     </div>
   );
